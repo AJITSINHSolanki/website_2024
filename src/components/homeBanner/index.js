8c726115b039d8ab0c8afa6c50f132e6549b4d45
Riddhi Chakole <rkalambe1309@gmail.com>
2024-02-23 14:05:50 +0530

    Home banner height adjust

--- src/components/homeBanner/index.js
+++ src/components/homeBanner/index.js
@@ -1,54 +1,42 @@
 import React, { useRef, useState } from 'react';
 import { HomeBannerCo } from '@/utils/constant';
 import Link from 'next/link';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Autoplay, Navigation } from 'swiper/modules';
 import Image from 'next/image';
-import HomeMain from "../home";
+import HomeMain from "../home"; 
 
 const HomeBanner = () => {
+    const imageHeight = 600;  
+
     return (
         <div>
-        <Swiper
-            spaceBetween={0}
-            centeredSlides={true}
-            speed={5000}
-            // autoplay={{
-            //     delay: 3000,
-            //     disableOnInteraction: false,
-            // }}
-
-            navigation={true}
-            modules={[Autoplay, Navigation, EffectFade]}
-            loop={true}
-            // effect="fade"
-            className="mySwiper home_banner_slider"
-            id='home'
-        >
-            {HomeBannerCo?.map((item, key) => (
-                <SwiperSlide key={key}>
-                     {/* <div className='slider_item'> */}
-                     <Link href={item.buttonUrl}  className='images_box'>
-                            <Image src={item.images} alt="slider1" />
-                            </Link>
-                        
-                        {/* <div className='slider_item_content'>
-                            <p className='title'>{item.title}</p>
-                            <p className='sub_text'>{item.content}</p>
-                            <Link href={item.buttonUrl} className='button_p'><span>{item.buttonName}</span></Link>
-                        </div> */}
-                    {/* </div> */}
-                </SwiperSlide>
-            ))}
-            
-        </Swiper>
-        <div>
-            <HomeMain/>
+            <Swiper
+                spaceBetween={0}
+                centeredSlides={true}
+                speed={5000}
+                navigation={true}
+                modules={[Autoplay, Navigation, EffectFade]}
+                loop={true}
+                className="mySwiper home_banner_slider"
+                id='home'
+            >
+                {HomeBannerCo?.map((item, key) => (
+                    <SwiperSlide key={key}>
+                        <Link href={item.buttonUrl} className='images_box'>
+                            <Image
+                                src={item.images}
+                                alt="slider1"
+                                style={{ height: `${imageHeight}px` }}
+                            />
+                        </Link>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            <div>
+                <HomeMain />
+            </div>
         </div>
-        
-        </div>
-        
     );
 }
 
