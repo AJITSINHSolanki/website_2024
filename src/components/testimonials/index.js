de8c2edd41702936b9a011282b104368ddf398cc
Dhaval <dhavaldodiya96@gmail.com>
2024-03-29 15:41:44 +0530

    Testimonials.js  -Client review section comments. globals.css  -client section, CSS, and contact Uus CSS sum changes.

--- src/components/testimonials/index.js
+++ src/components/testimonials/index.js
@@ -49,119 +49,120 @@ const Testimonials = () => {
   }, []);
 
   return (
-    <div
-      ref={myRef}
-      className={`testimonials_section fade-in ${isVisible ? "active" : ""}`}
-    >
-      <div className="container">
-        <div className="testimonials_ttp">
-          <p className="sub_title">Testimonial</p>
-          <p className="main_title">
-            What Our Clients <span>Say</span>
-          </p>
-        </div>
-        <div className="testimonial_list">
-          {TestimonialsInfo.length > 0 ? (
-            TestimonialsInfo?.map((item, key) => (
-              <div key={key} className="testimonial-item">
-                <div className="testimonial_inner">
-                  <p className="description">“{item.content}”</p>
-                  <div className="author-box">
-                    <div className="img_box">
-                      <Image src={item.images} alt={item.name} />
-                    </div>
-                    <div className="author-content">
-                      <p className="name">{item.name}</p>
-                      <p className="dissertation">{item.dissertation}</p>
-                    </div>
-                  </div>
-                  <div className="quote-icon">
-                    <Image src={QuorteImg} alt="QuorteImg" />
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <PageLoading />
-          )}
-          {/* {TestimonialsInfo?.map((item, key) => (
-                        <div key={key} className="testimonial-item">
-                            <div className="testimonial_inner">
-                                <p className="description">“{item.content}”</p>
-                                <div className='author-box'>
-                                    <div className="img_box">
-                                        <Image src={item.images} alt={item.name} />
-                                    </div>
-                                    <div className="author-content">
-                                        <p className='name'>{item.name}</p>
-                                        <p className='dissertation'>{item.dissertation}</p>
-                                    </div>
-                                </div>
-                                <div className='quote-icon'>
-                                    <Image src={QuorteImg} alt="QuorteImg" />
-                                </div>
-                            </div>
-                        </div>
-                    ))} */}
-        </div>
-        {/* <Swiper
-                    spaceBetween={20}
-                    slidesPerView={1}
-                    centeredSlides={false}
-                    breakpoints={{
-                        768: {
-                            slidesPerView: 1,
-                            spaceBetween: 20,
-                        },
-                        1024: {
-                            slidesPerView: 3,
-                            spaceBetween: 20,
-                        },
-                    }}
-                    speed={5000}
-                    autoplay={{
-                        delay: 3000,
-                        disableOnInteraction: false,
-                    }}
+    <></>
+    // <div
+    //   ref={myRef}
+    //   className={`testimonials_section fade-in ${isVisible ? "active" : ""}`}
+    // >
+    //   <div className="container">
+    //     <div className="testimonials_ttp">
+    //       <p className="sub_title">Testimonial</p>
+    //       <p className="main_title">
+    //         What Our Clients <span>Say</span>
+    //       </p>
+    //     </div>
+    //     <div className="testimonial_list">
+    //       {TestimonialsInfo.length > 0 ? (
+    //         TestimonialsInfo?.map((item, key) => (
+    //           <div key={key} className="testimonial-item">
+    //             <div className="testimonial_inner">
+    //               <p className="description">“{item.content}”</p>
+    //               <div className="author-box">
+    //                 <div className="img_box">
+    //                   <Image src={item.images} alt={item.name} />
+    //                 </div>
+    //                 <div className="author-content">
+    //                   <p className="name">{item.name}</p>
+    //                   <p className="dissertation">{item.dissertation}</p>
+    //                 </div>
+    //               </div>
+    //               <div className="quote-icon">
+    //                 <Image src={QuorteImg} alt="QuorteImg" />
+    //               </div>
+    //             </div>
+    //           </div>
+    //         ))
+    //       ) : (
+    //         <PageLoading />
+    //       )}
+    //       {/* {TestimonialsInfo?.map((item, key) => (
+    //                     <div key={key} className="testimonial-item">
+    //                         <div className="testimonial_inner">
+    //                             <p className="description">“{item.content}”</p>
+    //                             <div className='author-box'>
+    //                                 <div className="img_box">
+    //                                     <Image src={item.images} alt={item.name} />
+    //                                 </div>
+    //                                 <div className="author-content">
+    //                                     <p className='name'>{item.name}</p>
+    //                                     <p className='dissertation'>{item.dissertation}</p>
+    //                                 </div>
+    //                             </div>
+    //                             <div className='quote-icon'>
+    //                                 <Image src={QuorteImg} alt="QuorteImg" />
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                 ))} */}
+    //     </div>
+    //     {/* <Swiper
+    //                 spaceBetween={20}
+    //                 slidesPerView={1}
+    //                 centeredSlides={false}
+    //                 breakpoints={{
+    //                     768: {
+    //                         slidesPerView: 1,
+    //                         spaceBetween: 20,
+    //                     },
+    //                     1024: {
+    //                         slidesPerView: 3,
+    //                         spaceBetween: 20,
+    //                     },
+    //                 }}
+    //                 speed={5000}
+    //                 autoplay={{
+    //                     delay: 3000,
+    //                     disableOnInteraction: false,
+    //                 }}
 
-                    navigation={false}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    modules={[Navigation, Autoplay, Pagination,]}
-                    loop={true}
-                    effect="fade"
+    //                 navigation={false}
+    //                 pagination={{
+    //                     clickable: true,
+    //                 }}
+    //                 modules={[Navigation, Autoplay, Pagination,]}
+    //                 loop={true}
+    //                 effect="fade"
 
-                    className="mySwiper testimonial_list">
-                    {TestimonialsInfo?.map((item, key) => (
-                        <SwiperSlide key={key} className="testimonial-item">
-                            <div className="testimonial_inner">
-                                <div className="rating">
-                                    <span className='icon'><StarIcon /></span>
-                                    <span className='icon'><StarIcon /></span>
-                                    <span className='icon'><StarIcon /></span>
-                                    <span className='icon'><StarIcon /></span>
-                                    <span className='icon'><StarIcon /></span>
-                                </div>
-                                <p className="description">“{item.content}”</p>
-                                <div className='author-box'>
-                                    <div className="img_box">
-                                        <Image src={item.images} alt={item.name} />
-                                    </div>
-                                    <div className="author-content">
-                                        <p className='name'>{item.name}</p>
-                                        <p className='dissertation'>{item.dissertation}</p>
-                                    </div>
-                                </div>
-                                <div className='quote-icon'>
-                                    <Image src={QuorteImg} alt="QuorteImg" />
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper> */}
-      </div>
-    </div>
+    //                 className="mySwiper testimonial_list">
+    //                 {TestimonialsInfo?.map((item, key) => (
+    //                     <SwiperSlide key={key} className="testimonial-item">
+    //                         <div className="testimonial_inner">
+    //                             <div className="rating">
+    //                                 <span className='icon'><StarIcon /></span>
+    //                                 <span className='icon'><StarIcon /></span>
+    //                                 <span className='icon'><StarIcon /></span>
+    //                                 <span className='icon'><StarIcon /></span>
+    //                                 <span className='icon'><StarIcon /></span>
+    //                             </div>
+    //                             <p className="description">“{item.content}”</p>
+    //                             <div className='author-box'>
+    //                                 <div className="img_box">
+    //                                     <Image src={item.images} alt={item.name} />
+    //                                 </div>
+    //                                 <div className="author-content">
+    //                                     <p className='name'>{item.name}</p>
+    //                                     <p className='dissertation'>{item.dissertation}</p>
+    //                                 </div>
+    //                             </div>
+    //                             <div className='quote-icon'>
+    //                                 <Image src={QuorteImg} alt="QuorteImg" />
+    //                             </div>
+    //                         </div>
+    //                     </SwiperSlide>
+    //                 ))}
+    //             </Swiper> */}
+    //   </div>
+    // </div>
   );
 };
 
